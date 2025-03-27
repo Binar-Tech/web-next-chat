4110ab393a9f91ba775b5378e85794c3034bd86b
Marcos Roberto Albrecht <marquinhos.beto@hotmail.com>
2025-03-27 17:00:08 -0300

    fix: alter get datetime to user

--- app/utils/data.ts
+++ app/utils/data.ts
@@ -1,27 +1,27 @@
 export function formatDate(dateString: string) {
+  // Cria a data original recebida da API
   const date = new Date(dateString);
-  console.log("DATA: ", date);
-  console.log("DATA API: ", dateString);
-  const dateNow = new Date();
 
-  // Verifica se a data é de hoje
+  // Adiciona 3 horas à data original (por exemplo, para corrigir a diferença de fuso)
+  date.setHours(date.getHours() + 3); // Ajuste de 3 horas a mais
+
+  // Verifica o fuso horário do cliente (exemplo: Brasília está GMT -3)
+  const dateNow = new Date();
   const isToday =
     date.getDate() === dateNow.getDate() &&
     date.getMonth() === dateNow.getMonth() &&
     date.getFullYear() === dateNow.getFullYear();
 
-  // Definir fuso horário para o Brasil
-  const timeZone = "America/Sao_Paulo";
-
+  // Se a data for de hoje, retorna apenas a hora
   if (isToday) {
     return date.toLocaleTimeString("pt-BR", {
       hour: "2-digit",
       minute: "2-digit",
       hour12: false,
-      // Define explicitamente o fuso horário para o Brasil
     });
   }
 
+  // Caso contrário, retorna a data completa no formato desejado
   return date
     .toLocaleString("pt-BR", {
       day: "2-digit",
@@ -30,7 +30,6 @@ export function formatDate(dateString: string) {
       hour: "2-digit",
       minute: "2-digit",
       hour12: false,
-      // Define explicitamente o fuso horário para o Brasil
     })
     .replace(",", "");
 }
