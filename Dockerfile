e27ebd511a40abb6a003d85c468cbe5c9cf77d77
Marcos Roberto Albrecht <marquinhos.beto@hotmail.com>
2025-03-27 11:35:49 -0300

    fix: tsconfig

--- Dockerfile
+++ Dockerfile
@@ -17,7 +17,7 @@ RUN npm run build
 # Instala o PM2 globalmente
 RUN npm install -g pm2
 
-# Expõe a porta 4000
+# Expõe a porta 4001
 EXPOSE 4001
 
 # Inicia o servidor com PM2 na porta específica
--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
+
   experimental: {
     serverActions: {
       bodySizeLimit: "50mb",
     },
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   images: {
